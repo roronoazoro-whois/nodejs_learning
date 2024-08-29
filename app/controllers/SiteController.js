2e5303f47eebaa5aee67c82a82b0176973387590
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-08-29 15:25:13 +0700

    CRUD #1

--- app/controllers/SiteController.js
+++ app/controllers/SiteController.js
@@ -1,12 +1,17 @@
 const Course = require("../models/Course");
 
+const { mutipleMongooseToObject } = require("../../util/mongoose");
+
 class SiteController {
   // [GET] /
   async index(req, res) {
     try {
       // Sử dụng async/await để lấy dữ liệu
-      const courses = await Course.find({});
-      res.render("home", { courses });
+      let courses = await Course.find({});
+      // courses = courses.map((course) => course.toObject());
+      res.render("home", {
+        courses: mutipleMongooseToObject(courses),
+      });
     } catch (error) {
       res.status(400).json({ error: "ERROR!!!" });
     }
