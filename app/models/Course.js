f4e63288150c2e33db12117847aa40edd277d6b3
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-08-30 18:45:03 +0700

    AutoIncrease

--- app/models/Course.js
+++ app/models/Course.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const slug = require("mongoose-slug-updater");
 const mongooseDelete = require("mongoose-delete");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema(
   {
+    _id: { type: Number},
     name: { type: String, required: true },
     description: { type: String },
     image: { type: String },
@@ -14,10 +16,10 @@ const CourseSchema = new Schema(
     slug: { type: String, slug: "name", unique: true },
   },
   {
+    _id: false,
     timestamps: true,
   }
 );
-
 
 CourseSchema.query.sortable = function (req) {
   if (req.query.hasOwnProperty("_sort")) {
@@ -31,6 +33,7 @@ CourseSchema.query.sortable = function (req) {
 
 // Add plugins
 mongoose.plugin(slug);
+CourseSchema.plugin(AutoIncrement);
 CourseSchema.plugin(mongooseDelete, {
   deletedAt: true,
   overrideMethods: "all",
